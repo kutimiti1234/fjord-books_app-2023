f11de1a6771217039e2ddaf7537baa5b2db89dc5
kutimiti1234 <tarbit.offensive@gmail.com>
2024-04-07 21:54:28 +0900

    明示的に英語と日本語を許可ロケールへ追加

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module BooksApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
   end
 end
