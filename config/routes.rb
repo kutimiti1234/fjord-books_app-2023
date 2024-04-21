40a0dcbdd9b1b25ed4f253c237d3f62bed1d2178
kutimiti1234 <tarbit.offensive@gmail.com>
2024-04-21 11:46:23 +0900

    letter_opnerを追加するための設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users, except: %i[destroy new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # Defines the root path route ("/")
   # root "articles#index"
 end
