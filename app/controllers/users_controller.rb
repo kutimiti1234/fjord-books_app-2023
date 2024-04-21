20124847897b9b50f7ba92855202a300ee3d95a6
kutimiti1234 <tarbit.offensive@gmail.com>
2024-04-21 09:38:00 +0900

    ユーザーの編集機能の追加。ユーザー自身のプロフィールのみ編集可能に制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,30 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+    return if @user == current_user
+
+    redirect_to user_url(@user), notice: t('controllers.common.alert_update', name: User.model_name.human)
+  end
+
+  #
+  # PATCH/PUT /users/1 or /users/1.json
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: t('controllers.common.notice_update', name: User.model_name.human) }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, :users
+  resources :books
+  resources :users, except: %i[destroy new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
