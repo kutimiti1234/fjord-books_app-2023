4fe3c0cac3f11accd5694f340fa4257f150eb5fc
kutimiti1234 <tarbit.offensive@gmail.com>
2024-04-28 16:27:11 +0900

    不要なアクション、処理を削除。また、bookモデルと同様にbefore_actionにset_userを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,29 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.order(:id).page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
+  def show; end
 
   def edit
-    @user = User.find(params[:id])
     return if @user == current_user
 
     redirect_to user_url(@user), notice: t('controllers.common.alert_update', name: User.model_name.human)
   end
 
-  #
   # PATCH/PUT /users/1 or /users/1.json
   def update
+    return if @user == current_user
+
+    redirect_to user_url(@user), notice: t('controllers.common.notice_update', name: User.model_name.human)
+  end
+
+  def set_user
     @user = User.find(params[:id])
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: t('controllers.common.notice_update', name: User.model_name.human) }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def user_params
