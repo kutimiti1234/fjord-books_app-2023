cbf3c1620ca5105e7887728a2f795d48660f4383
kutimiti1234 <tarbit.offensive@gmail.com>
2024-05-04 17:22:31 +0900

    i18n対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
   resources :books
   resources :users, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
