c5dfca202855f01f8a8ab9f9066c0afb19ef5f57
kutimiti1234 <tarbit.offensive@gmail.com>
2024-05-07 18:48:51 +0900

    アイコンを設定しなかった場合にバリデーションに引っかからないように

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   private
 
   def correct_icon_type
+    return if !icon.attached?
+
     errors.add(:icon, I18n.t('errors.validate_mime_type')) unless icon.content_type.in?(ALLOWED_CONTENT_TYPES)
   end
 end
