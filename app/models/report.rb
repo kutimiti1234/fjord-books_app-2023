6f705ba1e391911f9edfbe7d020b4060fbc34d57
kutimiti1234 <tarbit.offensive@gmail.com>
2024-06-13 01:25:15 +0900

    関数の名前と変数名をわかりやすく変更。また、不要となったmentioned_reportsとmentioned_reports_idsの関数を削除

--- app/models/report.rb
+++ app/models/report.rb
@@ -21,29 +21,21 @@ class Report < ApplicationRecord
     created_at.to_date
   end
 
-  def mentioned_reports
-    ReportMention.where(mentioned_report: id).map { |mention| Report.find(mention.report_id) }
-  end
-
-  def mentioned_reports_ids
-    mentioned_reports.map(&:report_id)
-  end
-
-  def parse_url_in_content(content)
+  def parse_url_in_content
     r = %r{http://localhost:3000/reports/(\d+)}
     content.scan(r).flatten.map(&:to_i)
   end
 
   def update_report_mentions
-    current_urls = parse_url_in_content(content)
-    existing_urls = mentioning_report_ids
+    current_ids = parse_url_in_content
+    existing_ids = mentioning_report_ids
 
-    (current_urls - existing_urls).each do |mentioning_report_id|
+    (current_ids - existing_ids).each do |mentioning_report_id|
       mentioning_report = Report.find_by(id: mentioning_report_id)
       mentioning_reports << mentioning_report if mentioning_report
     end
 
-    (existing_urls - current_urls).each do |mentioning_report_id|
+    (existing_ids - current_ids).each do |mentioning_report_id|
       mentioning_report = Report.find_by(id: mentioning_report_id)
       mentioning_reports.delete(mentioning_report) if mentioning_report
     end
