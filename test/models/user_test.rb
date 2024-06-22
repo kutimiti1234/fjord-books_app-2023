197f057e023e0237937043ef81b8b4b0f49c6f2b
kutimiti1234 <tarbit.offensive@gmail.com>
2024-06-22 20:30:16 +0900

    ユーザーモデルのテストを実装

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'show name if name exsit ' do
+    assert_equal 'david', users(:david).name_or_email
+  end
+
+  test 'show email if name not exsit ' do
+    assert_equal 'example2@gmail.com', users(:alice).name_or_email
+  end
 end
