bc9bb87e3a4da92f72c2135945df42d39c397e96
kutimiti1234 <tarbit.offensive@gmail.com>
2024-06-23 17:35:52 +0900

    save_mentionsのprivateメソッドをpublicメソッドから間接的にテスト。また、テストの内容が見やすいように修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,13 +4,21 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test 'editable or not editable ' do
-    assert reports(:one).editable?(users(:david))
-    assert_not reports(:one).editable?(users(:alice))
+    report = Report.new(user_id: 1)
+    assert report.editable?(users(:one))
+    assert_not reports(:one).editable?(users(:two))
   end
 
   test 'the date of created_at' do
-    report = reports(:one)
-    date = report.created_at
+    date = DateTime.new(2023, 6, 23, 12, 34, 56)
+    report = Report.new(title: 'example', created_at: date)
     assert_equal date.to_date, report.created_on
+  end
+
+  test 'report_mentioning' do
+    report = Report.new(title: 'example', content: 'http://localhost:3000/reports/2', user_id: 1)
+    assert_not_equal [2], report.mentioning_report_ids
+    report.save
+    assert_equal [2], report.mentioning_report_ids
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,9 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'show name if name exsit or show email if name not exist ' do
-    assert_equal 'david', users(:david).name_or_email
-    assert_equal 'example2@gmail.com', users(:alice).name_or_email
+    user = User.new(email: 'example@gmail.com', name: 'david')
+    user_without_name = User.new(email: 'example@gmail.com')
+    assert_equal 'david', user.name_or_email
+    assert_equal 'example@gmail.com', user_without_name.name_or_email
   end
 end
