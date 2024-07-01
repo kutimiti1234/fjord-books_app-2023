e1d7f830b38157118f041e39fd4f77746f2dc3b3
kutimiti1234 <tarbit.offensive@gmail.com>
2024-07-01 23:52:04 +0900

    modelテストにfixtureを使うように設定。インスタンスメソッド単位のテストケースに変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,21 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test 'editable or not editable ' do
-    bob = User.new(email: 'example@example.com')
+  test '#editable?' do
+    bob = users(:bob)
     alice = User.new(email: 'example@example.com')
-    report = Report.new(user: bob)
+    report = reports(:one)
     assert report.editable?(bob)
     assert_not report.editable?(alice)
   end
 
-  test 'the date of created_at' do
+  test '#created_on' do
     date = DateTime.new(2023, 6, 23, 12, 34, 56)
     report = Report.new(title: 'example', created_at: date)
     assert_equal date.to_date, report.created_on
   end
 
-  test 'report_mentioning' do
+  test '#report_mentioning' do
     report = Report.new(title: 'example', content: 'http://localhost:3000/reports/2', user_id: 1)
     assert_not_equal [2], report.mentioning_report_ids
     report.save
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,10 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'show name if name exsit or show email if name not exist ' do
-    user = User.new(email: 'example@example.com', name: 'david')
-    user_without_name = User.new(email: 'example@example.com')
-    assert_equal 'david', user.name_or_email
-    assert_equal 'example@example.com', user_without_name.name_or_email
+  test '#name_or_email' do
+    user_without_name = users(:alice)
+    assert_equal 'example2@example.com', user_without_name.name_or_email
+
+    user = users(:bob)
+    assert_equal 'bob', user.name_or_email
   end
 end
